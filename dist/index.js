855a9cad64e9dc22fe7333854e4ae82edc690ae0
noorsiddiqui-prog <noorsiddiqui048@gmail.com>
2024-07-16 21:05:56 +0500

    configuration for deployement

--- dist/index.js
+++ dist/index.js
@@ -8,9 +8,11 @@
 // import cors from "cors"
 // import { app, server } from "./socket/socket.js";
 // import path from "path"
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.server = exports.app = void 0;
 // dotenv.config()
@@ -38,7 +40,9 @@ exports.server = exports.app = void 0;
 // })
 const express_1 = __importDefault(require("express"));
 const auth_route_js_1 = __importDefault(require("./routes/auth/auth.route.js"));
-const message_route_js_1 = __importDefault(require("./routes/message/message.route.js"));
+const message_route_js_1 = __importDefault(
+  require("./routes/message/message.route.js")
+);
 const dotenv_1 = __importDefault(require("dotenv"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
@@ -48,22 +52,30 @@ dotenv_1.default.config();
 const app = (0, express_1.default)();
 exports.app = app;
 const PORT = process.env.PORT || 5000;
-app.use((0, cors_1.default)({
-    origin: '*', // Replace with your frontend URL
-    credentials: true // Enable cookies and other credentials
-}));
+app.use(
+  (0, cors_1.default)({
+    origin: "*", // Replace with your frontend URL
+    credentials: true, // Enable cookies and other credentials
+  })
+);
 app.use((0, cookie_parser_1.default)()); // parse cookies from request headers
 app.use(express_1.default.json()); // for parsing application/json
 app.use(body_parser_1.default.urlencoded({ extended: true }));
 app.use("/api/auth", auth_route_js_1.default);
 app.use("/api/messages", message_route_js_1.default);
-if (process.env.NODE_ENV !== 'development') {
-    app.use(express_1.default.static(path_1.default.join(process.cwd(), 'frontend', 'dist')));
-    app.get("*", (req, res) => {
-        res.sendFile(path_1.default.resolve(process.cwd(), 'frontend', 'dist', 'index.html'));
-    });
+if (process.env.NODE_ENV !== "development") {
+  app.use(
+    express_1.default.static(
+      path_1.default.join(process.cwd(), "frontend", "dist")
+    )
+  );
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path_1.default.resolve(process.cwd(), "frontend", "dist", "index.html")
+    );
+  });
 }
 const server = app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
 exports.server = server;
